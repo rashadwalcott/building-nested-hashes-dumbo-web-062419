c50a2f863fa58ffd772fa83cf3a49ffc173ac6ed
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 02:28:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,45 +3,45 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
+        :name "Lord Montague",
+        :age "53"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Montague",
-        :age => "54"
+        :name "Lady Montague",
+        :age "54"
         },
       :hero => {
         #your key/value pairs here
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
+        :name "Romeo",
+        :age "15",
+        :status "alive"
         },
       :hero_friends => [
-        {:name => "Benvolio",:age => "17", :attitude => "worried"}
-        {:name => "Mercutio", :age => "18",:attitude => "hot-headed"}
+        {:name "Benvolio",:age "17", :attitude "worried"}
+        {:name "Mercutio", :age "18",:attitude "hot-headed"}
         ]
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Capulet",
-        :age => "50"
+        :name "Lord Capulet",
+        :age "50"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Capulet",
-        :age => "51"
+        :name "Lady Capulet",
+        :age "51"
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
+        :name "Juliet",
+        :age "15",
+        :status "alive"
         },
       :heroine_friends => [
-        {:name => "Steven",:age => "44", :attitude => "confused"}
-        {:name => "Nurse", :age => "44", :attitude = "worried"}
+        {:name "Steven",:age "44", :attitude "confused"}
+        {:name "Nurse", :age "44", :attitude "worried"}
         ]
    }
   }
